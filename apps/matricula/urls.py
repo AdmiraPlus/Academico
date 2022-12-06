0ec26bed0d1429f6ca944e19bb4f6e5364c663c7
Leoncio R. Barrios H <leonciobarriosh@gmail.com>
2022-12-06 13:34:43 -0500

    Pequeños ajustes a plantillas y seguimiento en git de la bd SQLite

--- apps/matricula/urls.py
+++ apps/matricula/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('buscar/', BuscarAlumno, name='buscar'),
+	path('buscar', BuscarAlumno, name='buscar'),
 	
 	path('pre_matricula/<str:codigo>/', PreMatricula, name='pre_matricula'),
 	#path('pre_matricula/', PreMatricula, {'datos':'alumno'}, name='pre_matricula'),
